fa62acb24548b7ae1d41191c80d1f451125b45e6
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2026-03-21 11:34:27 +0200

    add new colors to the moving design element to the hero

--- src/components/HeroBlob.tsx
+++ src/components/HeroBlob.tsx
@@ -55,12 +55,12 @@ export const HeroBlob = () => {
 
       <defs>
         <linearGradient id="grad1" x1="420" y1="0" x2="420" y2="1150" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#EAEAEB" stopOpacity="0.1" />
-          <stop offset="1" stopColor="#EAEAEB" />
+          <stop stopColor="#B4B4BB" stopOpacity="0.1" />
+          <stop offset="1" stopColor="#B4B4BB" />
         </linearGradient>
         <linearGradient id="grad2" x1="1000" y1="350" x2="1000" y2="1550" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#EAEAEB" />
-          <stop offset="1" stopColor="#EAEAEB" stopOpacity="0.1" />
+          <stop stopColor="#B4B4BB" />
+          <stop offset="1" stopColor="#B4B4BB" stopOpacity="0.1" />
         </linearGradient>
       </defs>
     </svg>
